5fdc38901892617ebc972ed339a9545bb3803642
CristianoSFMothe <cristianodevsystem@gmail.com>
2023-08-25 22:07:37 -0300

    :up: refatorando a logica

--- abrir_chamado.php
+++ abrir_chamado.php
@@ -1,10 +1,5 @@
-<?php
-  session_start();
-
-  if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
-    header('Location: index.php?login=erro_login');
-  }
-
+<?php 
+require_once "validador_acesso.php";
 ?>
 
 <!DOCTYPE html>
@@ -24,7 +19,7 @@
 
   <nav class="navbar navbar-dark bg-dark">
     <a class="navbar-brand" href="#">
-      <img src="./img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+      <img src="./assents/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
       App Help Desk
     </a>
   </nav>
--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,10 +1,5 @@
-<?php
-  session_start();
-
-  if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
-    header('Location: index.php?login=erro_login');
-  }
-
+<?php 
+require_once "validador_acesso.php";
 ?>
 
 <!DOCTYPE html>
